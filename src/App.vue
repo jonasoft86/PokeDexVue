c2ba54feb678514570f9113b463ace24e3205afb
Jonasoft <jonathan.monroy@live.com>
2024-12-01 14:23:43 -0300

    initial project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Home from './pages/Home.vue';
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style lang="scss">
+  @use "./assets/scss/main.scss";
+  @use "./assets/scss/global.scss"
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <aside>
+        </aside>
+        <main>
+            <h2 class="title" id="title">Lista Pokemones</h2>
+            <div id="container" class="container">
+            </div>
+        </main>
+     </div>
+</template>
+
+<style lang="scss">
+    @use "../assets/scss/pages/home";
+</style>
+
